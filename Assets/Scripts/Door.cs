07a3825f29402b497b01edcb65fe7fd5bfd2e0d3
Adrian Epifanio Rodriguez <alu0101158280@ull.edu.es>
2022-01-22 10:47:19 +0000

    añadidos textos norte-sur-este-oeste

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+    bool opened_ = false;
+    public GameObject TextObject;
+    
+    // Start is called before the first frame update
+    void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
+
+    /*=====================================
+    =            Pointer Enter            =
+    =====================================*/
+    
+    public void InfoDoorNorth () {
+        if (TextObject) {
+            GameObject text = Instantiate(TextObject);
+            text.transform.position = new Vector3(this.gameObject.transform.position.x - 1, this.gameObject.transform.position.y + 1, this.gameObject.transform.position.z);
+        }
+        Debug.Log("Next room: ...");
+    }
+
+    public void InfoDoorSouth () {
+        if (TextObject) {
+            GameObject text = Instantiate(TextObject);
+            text.transform.position = new Vector3(this.gameObject.transform.position.x + 1, this.gameObject.transform.position.y + 1, this.gameObject.transform.position.z);
+        }
+        Debug.Log("Next room: ...");
+    }
+
+    public void InfoDoorWest () {
+        if (TextObject) {
+            GameObject text = Instantiate(TextObject);
+            text.transform.position = new Vector3(this.gameObject.transform.position.x + 1, this.gameObject.transform.position.y + 1, this.gameObject.transform.position.z - 1);
+        }
+        Debug.Log("Next room: ...");
+    }
+
+    public void InfoDoorEast () {
+        if (TextObject) {
+            GameObject text = Instantiate(TextObject);
+            text.transform.position = new Vector3(this.gameObject.transform.position.x + 1, this.gameObject.transform.position.y + 1, this.gameObject.transform.position.z + 1);
+        }
+        Debug.Log("Next room: ...");
+    }
+    
+    /*=====  End of Pointer Enter  ======*/
+    
+    
+    /*=====================================
+    =            Pointer Click            =
+    =====================================*/
+    
+    public void OpenDoorNorth () {
+        opened_ = true;
+        Debug.Log("AbrirPuertaNorte");
+    }
+
+    public void OpenDoorSouth () {
+        opened_ = true;
+        Debug.Log("AbrirPuertaSur");
+    }
+
+    public void OpenDoorWest () {
+        opened_ = true;
+        Debug.Log("AbrirPuertaOeste");
+    }
+
+    public void OpenDoorEast () {
+        opened_ = true;
+        Debug.Log("AbrirPuertaEste");
+    }
+    
+    /*=====  End of Pointer Click  ======*/
+    
+   
+    /*====================================
+    =            Pointer Exit            =
+    ====================================*/
+    
+    public void CloseDoorNorth () {
+        if (opened_) {
+            opened_ = false;
+            Debug.Log("CerrarPuertaNorte");
+        }
+        
+    }
+
+    public void CloseDoorSouth () {
+        if (opened_) {
+            opened_ = false;
+            Debug.Log("CerrarPuertaSur");
+        }   
+    }
+
+    public void CloseDoorWest () {
+        if (opened_) {
+            opened_ = false;
+            Debug.Log("CerrarPuertaOeste");
+        }
+        
+    }
+
+    public void CloseDoorEast () {
+        if (opened_) {
+            opened_ = false;
+            Debug.Log("CerrarPuertaEste");
+        }
+        
+    }
+    
+    /*=====  End of Pointer Exit  ======*/
+        
+}
---
+++ Assets/Scripts/TextSettings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextSettings : MonoBehaviour {
+
+    float aliveTime = 2;
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        aliveTime -= Time.deltaTime;
+        if (aliveTime <= 0) {
+            Destroy(this.gameObject);
+        }
+    }
+}
